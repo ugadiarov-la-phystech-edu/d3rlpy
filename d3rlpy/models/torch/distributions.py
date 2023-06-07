fd1cfea69b2d7eb182710d31f927e1dc1d713ae1
Petr Kuderov <pkuderov@gmail.com>
2023-06-08 00:39:20 +0300

    finish clean up distributions.py

--- d3rlpy/models/torch/distributions.py
+++ d3rlpy/models/torch/distributions.py
@@ -91,74 +91,6 @@ class GaussianDistribution(Distribution):
         return self._std
 
 
-class GumbelDistribution(Distribution):
-        def __init__(self, probs, uniform_treshs, logits=None, temperature=1):
-            super().__init__()
-            if logits is not None:
-                self._logits = logits
-            self.probs = probs
-            self.eps = 1e-20
-            self.temperature = 1
-            self.uniform_treshs = uniform_treshs
-
-        def sample_gumbel(self):
-            U = torch.zeros_like(self.probs)
-            U.uniform_(*self.uniform_treshs)
-            to_gumbel = -torch.log(-torch.log(U + self.eps) + self.eps)
-            return to_gumbel
-
-        def gumbel_softmax_sample(self, logits = None):
-            """
-            Draw a sample from the Gumbel-Softmax distribution.
-            The returned sample will be a probability distribution
-            that sums to 1 across classes
-            """
-            y = self.probs + self.sample_gumbel()
-            out = torch.softmax(y / self.temperature, dim=-1)
-            return out
-
-        def sample_with_log_prob(self) -> Tuple[torch.Tensor, torch.Tensor]:
-            y = self.rsample()
-            lp = self.log_prob()
-            return y, lp
-
-        def hard_gumbel_softmax_sample(self):
-            y = self.gumbel_softmax_sample()
-            return (torch.max(y, dim=-1, keepdim=True)[0] == y).float()
-
-        def rsample(self):
-            return self.gumbel_softmax_sample().clamp(-1,1)
-
-        def sample(self):
-            return self.rsample().detach()
-
-        def sample_n(self, n: int) -> torch.Tensor:
-            samples = torch.from_numpy(np.array([self.rsample() for _ in range(n)]).reshape(n,))
-            return samples
-
-        def sample_n_with_log_prob(
-            self, n: int
-        ) -> Tuple[torch.Tensor, torch.Tensor]:
-            x = self.sample_n(n)
-            return x, self.log_prob(x)
-
-        def hard_sample(self):
-            out = self.hard_gumbel_softmax_sample()
-            return out
-
-        @property
-        def logits(self):
-            return self.probs#.max(axis = 1).values.reshape(-1,1)
-
-        @property
-        def logitss(self):
-            y = self.sample().max(axis = 1).values
-            return torch.log(y + self.eps)
-
-        def log_prob(self):
-            return  self.probs
-
-
 class SquashedGaussianDistribution(Distribution):
     _mean: torch.Tensor
     _std: torch.Tensor
@@ -212,6 +144,69 @@ class SquashedGaussianDistribution(Distribution):
     @property
     def std(self) -> torch.Tensor:
         return self._std
+
+
+class GumbelDistribution(Distribution):
+    def __init__(self, probs, uniform_treshs, logits=None, temperature=1):
+        super().__init__()
+        if logits is not None:
+            self._logits = logits
+        self.probs = probs
+        self.eps = 1e-20
+        self.temperature = temperature
+        self.uniform_treshs = uniform_treshs
+
+    def sample_gumbel(self):
+        U = torch.zeros_like(self.probs)
+        U.uniform_(*self.uniform_treshs)
+        to_gumbel = -torch.log(-torch.log(U + self.eps) + self.eps)
+        return to_gumbel
+
+    def gumbel_softmax_sample(self, logits = None):
+        """
+        Draw a sample from the Gumbel-Softmax distribution.
+        The returned sample will be a probability distribution
+        that sums to 1 across classes
+        """
+        y = self.probs + self.sample_gumbel()
+        out = torch.softmax(y / self.temperature, dim=-1)
+        return out
+
+    def sample_with_log_prob(self) -> Tuple[torch.Tensor, torch.Tensor]:
+        y = self.rsample()
+        lp = self.log_prob()
+        return y, lp
+
+    def hard_gumbel_softmax_sample(self):
+        y = self.gumbel_softmax_sample()
+        return (torch.max(y, dim=-1, keepdim=True)[0] == y).float()
+
+    def rsample(self):
+        return self.gumbel_softmax_sample().clamp(-1,1)
+
+    def sample(self):
+        return self.rsample().detach()
+
+    def sample_n(self, n: int) -> torch.Tensor:
+        samples = torch.from_numpy(np.array([self.rsample() for _ in range(n)]).reshape(n,))
+        return samples
+
+    def sample_n_with_log_prob(
+        self, n: int
+    ) -> Tuple[torch.Tensor, torch.Tensor]:
+        x = self.sample_n(n)
+        return x, self.log_prob(x)
+
+    def hard_sample(self):
+        out = self.hard_gumbel_softmax_sample()
+        return out
+
+    @property
+    def logits(self):
+        return self.probs
+
+    def log_prob(self, y=None):
+        return self.probs
 
 
 def gumbel_pdf(x, loc, scale):
